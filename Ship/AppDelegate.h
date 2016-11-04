2b103af2622a230e19295c2209a5a7712f7eb38e
wangzhiming <azraelWang@163.com>
2016-11-04 15:55:39 +0800

    Initial Commit

---
+++ Ship/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ship
+//
+//  Created by zhouying on 2016/11/4.
+//  Copyright © 2016年 azraelWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ship/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ship
+//
+//  Created by zhouying on 2016/11/4.
+//  Copyright © 2016年 azraelWang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
